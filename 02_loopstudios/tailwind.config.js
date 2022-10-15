3ebaa3d603ad604699b5bf0c7c4924f52072a3d5
천수민 <102133017+smthousand@users.noreply.github.com>
2022-10-15 19:15:23 +0900

    updated

---
+++ 02_loopstudios/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['SUIT Variable', 'sans-serif'],
+      },
+      letterSpacint: {
+        widest: '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
